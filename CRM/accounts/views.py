98187fcdeaee7fd2b849d7d8634626f71d4d924f
Yassine <yassineattari64@gmail.com>
2021-01-23 20:15:14 +0100

    [CRM] feat: Feed dashboard with life & added customer dynamic link to load it

--- CRM/accounts/views.py
+++ CRM/accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from accounts.models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    totalOrders = Order.objects.count()
+    deliveredOrders = Order.objects.filter(status='Delivered').count()
+    pendingOrders = Order.objects.filter(status='Pending').count()
+    context = {'orders': orders, 'customers': customers, 'totalOrders': totalOrders, 'deliveredOrders': deliveredOrders, 'pendingOrders': pendingOrders}
+    return render(request, 'accounts/dashboard.html',context=context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_customer):
+    customer = Customer.objects.get(id=pk_customer)
+    orders = customer.order_set.all()
+    totalOrdersCount = orders.count()
+    context = {'customer':customer, 'orders':orders, 'totalOrdersCount': totalOrdersCount}
+    return render(request, 'accounts/customer.html', context=context)
