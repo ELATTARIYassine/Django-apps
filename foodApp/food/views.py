da85c6624fd796ce070b1960c28d9f7850d9249b
Yassine <yassineattari64@gmail.com>
2021-03-06 12:59:43 +0100

    [food] feat: generate basic structure links, methods and config

--- foodApp/food/views.py
+++ foodApp/food/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+def index(request):
+    items = Item.objects.all()
+    context = {'items': items}
+    return render(request, "food/index.html", context=context)
+
+def item(request):
+    return "z"
+
+def detail(request, pk_item):
+    item = Item.objects.get(pk=pk_item)
+    context = {'item': item}
+    return render(request, "food/details.html", context=context)
