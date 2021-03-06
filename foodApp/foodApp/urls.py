587e5d04456e4ff5e27fb199dd5c548f833b00b5
Yassine <yassineattari64@gmail.com>
2021-03-06 20:04:54 +0100

    [food] feat: login and restricting routes

--- foodApp/foodApp/urls.py
+++ foodApp/foodApp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import register
+from users.views import register, profilePage
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include("food.urls")),
     path('register/', register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('profile/', profilePage, name='profile')
 ]
