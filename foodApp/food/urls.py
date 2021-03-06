f2809078611e8ac29ba95cb6dafd50be469426b4
Yassine <yassineattari64@gmail.com>
2021-03-06 18:17:10 +0100

    [food] feat: update and delete

--- foodApp/food/urls.py
+++ foodApp/food/urls.py
@@ -6,5 +6,7 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name="index"),
     path('<int:pk_item>/', views.detail, name="detail"),
-    path('add/', views.create_item, name="create_item")
+    path('add/', views.create_item, name="create_item"),
+    path('update/<int:pk_item>', views.update_item, name="update_item"),
+    path('delete/<int:pk_item>', views.delete_item, name="delete_item"),
 ]
