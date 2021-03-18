3b381107145af1072d21150d0d1e96117c223805
Yassine <yassineattari64@gmail.com>
2021-03-18 15:58:10 +0100

    [eco] feat: add static files and display products and add bootstrap

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    print(products)
+    return render(request, "shop/index.html", context={'products': products})
