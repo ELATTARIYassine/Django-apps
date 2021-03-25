02335fb83441fc753ad7d3d1289eebe9fef3cee5
Yassine <yassineattari64@gmail.com>
2021-03-25 16:29:22 +0100

    [blog] feat: add forms to add & edit and style

--- blog/adminSide/views.py
+++ blog/adminSide/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+def createPost(request):
+	form = PostForm()
+	if request.method == 'POST':
+		#print('Printing POST:', request.POST)
+		form = PostForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'adminSide/create_post.html', context)
+
+def updatePost(request, pk):
+
+	post = Post.objects.get(id=pk)
+	form = PostForm(instance=post)
+
+	if request.method == 'POST':
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'adminSide/create_post.html', context)
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from adminSide import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.createPost, name="index"),
+    path('<int:pk>/', views.updatePost, name="update"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
